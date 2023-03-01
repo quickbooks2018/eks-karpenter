ecf395b574b42c005fce61d5b747d61ed533e168
Muhammad Asim <quickbooks2018@gmail.com>
2023-03-01 02:40:05 +0000

    terraform eks iam mysql

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,15 @@ terraform {
         version = "4.55.0"
     }
 
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+
   }
 }
 
@@ -304,26 +313,30 @@ module "eks" {
 }
 
 # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
-data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_name
-}
-
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  # token                  = data.aws_eks_cluster_auth.default.token
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
-    command     = "aws"
-  }
-}
+#data "aws_eks_cluster" "default" {
+#  name = module.eks.cluster_name
+#  depends_on = [module.eks]
+#}
+#
+#data "aws_eks_cluster_auth" "default" {
+#  name = module.eks.cluster_name
+#  depends_on = [module.eks]
+#}
+#
+#
+#provider "kubernetes" {
+#  host                   = data.aws_eks_cluster.default.endpoint
+#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+#  # token                  = data.aws_eks_cluster_auth.default.token
+#
+#  exec {
+#    api_version = "client.authentication.k8s.io/v1beta1"
+#    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+#    command     = "aws"
+#  }
+#
+#  depends_on = [module.eks]
+#}
 
 ######
 # Mysql
